a354960e910165053c87f862b4caa312a889ee35
Denis Magda <dmagda@gridgain.com>
2022-03-21 13:58:44 -0400

    added the data folder

---
+++ data/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Account (
+    id int PRIMARY KEY,
+    name varchar,
+    age int,
+    country varchar,
+    balance int);
+
+INSERT INTO Account VALUES
+    (1, 'Jessica', 28, 'USA', 10000),
+    (2, 'John', 28, 'Canada', 9000);
