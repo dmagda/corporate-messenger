8bf49a813fa20e74537108137d81818bc2d6a57d
Denis Magda <magda7817@gmail.com>
2022-03-22 14:10:25 +0000

    added schema for the messaging table

--- data/messaging_service_schema.sql
+++ data/messaging_service_schema.sql
@@ -1,16 +1,6 @@
-CREATE SEQUENCE profile_id_seq CACHE 100 START WITH 10;
-CREATE SEQUENCE workspace_id_seq CACHE 100 START WITH 10;
-CREATE SEQUENCE channel_id_seq CACHE 100 START WITH 10;
-CREATE SEQUENCE message_id_seq CACHE 100 START WITH 15;
-
-CREATE TABLE Profile(
-    id integer NOT NULL DEFAULT nextval('profile_id_seq'),
-    full_name text NOT NULL,
-    email text NOT NULL,
-    phone text,
-    country text NOT NULL,
-    PRIMARY KEY(id, email, country)
-) PARTITION BY LIST(country);
+CREATE SEQUENCE workspace_id_seq CACHE 100;
+CREATE SEQUENCE channel_id_seq CACHE 100;
+CREATE SEQUENCE message_id_seq CACHE 100;
 
 CREATE TABLE Workspace(
     id integer NOT NULL DEFAULT nextval('workspace_id_seq'),
@@ -22,18 +12,18 @@ CREATE TABLE Workspace(
 
 CREATE TABLE WorkspaceProfile(
     workspace_id integer NOT NULL,
-    profile_email text NOT NULL,
-    country text NOT NULL,
-    PRIMARY KEY(workspace_id, profile_email, country)
-) PARTITION BY LIST(country);
+    profile_id integer NOT NULL,
+    workspace_country text NOT NULL,
+    PRIMARY KEY(workspace_id, profile_id, workspace_country)
+) PARTITION BY LIST(workspace_country);
 
 CREATE TABLE Channel(
     id integer NOT NULL DEFAULT nextval('channel_id_seq'),
     name text NOT NULL,
     workspace_id integer NOT NULL,
     is_direct BOOLEAN DEFAULT false,
-    direct_profile1_email text,
-    direct_profile2_email text,
+    direct_profile1_id integer,
+    direct_profile2_id integer,
     country text NOT NULL,
     PRIMARY KEY(id, country)
 ) PARTITION BY LIST(country);
@@ -42,7 +32,7 @@ CREATE TABLE Channel(
 CREATE TABLE Message(
     id integer NOT NULL DEFAULT nextval('message_id_seq'),
     channel_id integer,
-    sender_email text,
+    sender_id integer NOT NULL,
     message text NOT NULL,
     sent_at TIMESTAMP(0) DEFAULT NOW(),
     country text NOT NULL,
@@ -50,39 +40,11 @@ CREATE TABLE Message(
 ) PARTITION BY LIST(country);
 
 
-CREATE TABLESPACE americas_tablespace WITH (
-  replica_placement='{"num_replicas": 1, "placement_blocks":
-  [{"cloud":"gcp","region":"us-central1","zone":"us-central1-a","min_num_replicas":1}]}'
-);
-
-CREATE TABLESPACE europe_tablespace WITH (
-  replica_placement='{"num_replicas": 1, "placement_blocks":
-  [{"cloud":"gcp","region":"europe-west3","zone":"europe-west3-a","min_num_replicas":1}]}'
-);
-
-CREATE TABLESPACE asia_tablespace WITH (
-  replica_placement='{"num_replicas": 1, "placement_blocks":
-  [{"cloud":"gcp","region":"asia-east1","zone":"asia-east1-a","min_num_replicas":1}]}'
-);
-
-
-CREATE TABLE Profile_Americas
-    PARTITION OF Profile
-    FOR VALUES IN ('USA', 'Canada', 'Mexico', 'Brazil') TABLESPACE americas_tablespace;
-
-CREATE TABLE Profile_Europe
-    PARTITION OF Profile
-    FOR VALUES IN ('United Kingdom', 'France', 'Germany', 'Spain') TABLESPACE europe_tablespace;
-
-CREATE TABLE Profile_Asia
-    PARTITION OF Profile
-    FOR VALUES IN ('India', 'China', 'Japan', 'Australia') TABLESPACE asia_tablespace;
-
-
+--Note, the tablespaces are created in the profile_service_schema.sql
 
 CREATE TABLE Workspace_Americas
     PARTITION OF Workspace
-    FOR VALUES IN ('USA', 'Canada', 'Mexico', 'Brazil') TABLESPACE americas_tablespace;
+    FOR VALUES IN ('USA', 'Canada', 'Mexico') TABLESPACE americas_tablespace;
 
 CREATE TABLE Workspace_Europe
     PARTITION OF Workspace
@@ -96,7 +58,7 @@ CREATE TABLE Workspace_Asia
 
 CREATE TABLE WorkspaceProfile_Americas
     PARTITION OF WorkspaceProfile
-    FOR VALUES IN ('USA', 'Canada', 'Mexico', 'Brazil') TABLESPACE americas_tablespace;
+    FOR VALUES IN ('USA', 'Canada', 'Mexico') TABLESPACE americas_tablespace;
 
 CREATE TABLE WorkspaceProfile_Europe
     PARTITION OF WorkspaceProfile
@@ -110,7 +72,7 @@ CREATE TABLE WorkspaceProfile_Asia
 
 CREATE TABLE Channel_Americas
     PARTITION OF Channel
-    FOR VALUES IN ('USA', 'Canada', 'Mexico', 'Brazil') TABLESPACE americas_tablespace;
+    FOR VALUES IN ('USA', 'Canada', 'Mexico') TABLESPACE americas_tablespace;
 
 CREATE TABLE Channel_Europe
     PARTITION OF Channel
@@ -121,10 +83,9 @@ CREATE TABLE Channel_Asia
     FOR VALUES IN ('India', 'China', 'Japan', 'Australia') TABLESPACE asia_tablespace;
 
 
-
 CREATE TABLE Message_Americas
     PARTITION OF Message
-    FOR VALUES IN ('USA', 'Canada', 'Mexico', 'Brazil') TABLESPACE americas_tablespace;
+    FOR VALUES IN ('USA', 'Canada', 'Mexico') TABLESPACE americas_tablespace;
 
 CREATE TABLE Message_Europe
     PARTITION OF Message
@@ -133,3 +94,36 @@ CREATE TABLE Message_Europe
 CREATE TABLE Message_Asia
     PARTITION OF Message
     FOR VALUES IN ('India', 'China', 'Japan', 'Australia') TABLESPACE asia_tablespace;  
+
+
+    -- Load sample data
+
+INSERT INTO Workspace VALUES 
+(1, 'Apache Software Foundation', 'https://apache.org', 'USA'),
+(2, 'Shopify', 'https://shopify.com', 'Canada'),
+(3, 'SAP', 'https://sap.com/', 'Germany'),
+(4, 'TikTok', 'https://tiktok.com', 'China'),
+(5, 'Tech Mahindra', 'https://techmahindra.com', 'India');
+
+ALTER SEQUENCE workspace_id_seq RESTART WITH 6;
+
+INSERT INTO Channel VALUES
+(1, 'apache_kafka_support', 1, false, NULL, NULL, 'USA'),
+(2, 'apache_ignite_security', 1, false, NULL, NULL, 'USA'),
+(3, 'roadmap_suggestions', 2, false, NULL, NULL, 'Canada'),
+(4, 'sap_hana_dev', 3, false, NULL, NULL, 'Germany'),
+(5, 'vip_users_recognition', 4, false, NULL, NULL, 'China'),
+(6, 'viral_videos_ideas', 4, false, NULL, NULL, 'China'),
+(7, 'random', 5, false, NULL, NULL, 'India'),
+(8, 'on_call_customer_support', 5, false, NULL, NULL, 'India');
+
+ALTER SEQUENCE channel_id_seq RESTART WITH 9;
+
+INSERT INTO WorkspaceProfile VALUES
+(1, 1, 'USA'), (1, 5, 'USA'), -- ASF
+(1, 8, 'USA'), (1, 9, 'USA'), -- ASF
+(2, 2, 'Canada'), (2, 3, 'Canada'), -- Shopify
+(3, 4, 'Germany'), -- SAP
+(4, 6, 'China'), (4, 7, 'China'), -- TikTok
+(5, 9, 'India'), (5, 10, 'India'); -- Tech Mahindra
+
